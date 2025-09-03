aea6fb42e08e9cf02672bb0345dd8d9779e859b3
nttkor <mpeg4@ymail.com>
2025-09-03 13:06:07 +0900

    thread between time

---
+++ multiThread/threadTest1.py
@@ -0,0 +1,16 @@
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+def print_sum(num1, num2):
+    time.sleep(3)
+    print(num1 + num2, time.ctime())
+
+def main():
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        executor.submit(print_sum, 1, 2)
+        executor.submit(print_sum, 2, 3)
+        executor.submit(print_sum, 3, 4)
+    print("done!")
+
+if __name__ == "__main__":
+    main()
